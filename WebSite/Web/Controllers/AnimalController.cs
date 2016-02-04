bd823e2301412cb858bc7224bebe3d867424fd66
Anton Lukyanov <zerobotix@gmail.com>
2016-02-04 21:30:52 +0300

    created default project directories and pulled in Web project

---
+++ WebSite/Web/Controllers/AnimalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DajLapu.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: Animal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebSite/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DajLapu.MvcApplication" Language="C#" %>
