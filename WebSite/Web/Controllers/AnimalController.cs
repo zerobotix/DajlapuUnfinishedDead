cce5a9546b857e08a78cd08c0032053d7fd9819a
Anton Lukyanov <zerobotix@gmail.com>
2016-02-14 18:39:25 +0300

    Исправления в стилях на доске объявлений, немного накидал страницу найден

--- WebSite/Web/Controllers/AnimalController.cs
+++ WebSite/Web/Controllers/AnimalController.cs
@@ -4,19 +4,34 @@ namespace DajLapu.Web.Controllers
 {
     public class AnimalController : Controller
     {
-        // GET: Animal
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Add()
         {
             return View();
         }
+
+        [HttpPost]
+        public JsonResult Add(int x)
+        {
+            return Json(x);
+        }
+
+        [HttpGet]
         public ActionResult List()
         {
+            //var model = GetFilterOptions();
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult List(object x)
+        {
+            return Json(x);
         }
     }
 }
